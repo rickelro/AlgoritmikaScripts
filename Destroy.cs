34d8db19d25d0d9b6af03e0f10cef373ef5e167c
rickelro <119889671+rickelro@users.noreply.github.com>
2025-05-18 20:46:53 +0300

    Update Destroy.cs

--- Destroy.cs
+++ Destroy.cs
@@ -7,7 +7,7 @@ public class Destroy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // При старте удаляет объект, к которому прикреплен скрипт
+        // ГЏГ°ГЁ Г±ГІГ Г°ГІГҐ ГіГ¤Г Г«ГїГҐГІ Г®ГЎГєГҐГЄГІ, ГЄ ГЄГ®ГІГ®Г°Г®Г¬Гі ГЇГ°ГЁГЄГ°ГҐГЇГ«ГҐГ­ Г±ГЄГ°ГЁГЇГІ
         Destroy(gameObject);
     }
 
